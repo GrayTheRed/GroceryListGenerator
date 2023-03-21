973fa78e2fa9b61cb650ca0a9a9e718d2c603504
Joshua Gray <j.d.gray@outlook.com>
2023-03-21 12:49:55 -0700

    Adding Ingredient and Meal

---
+++ GroceryListGenerator/GroceryListGenerator/Ingredient.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroceryListGenerator
+{
+    class Ingredient
+    {
+        public string Name { get; set; }
+        public double Quantity { get; set; }
+        public MeasurementType Measurement { get; set; }
+
+        public Ingredient(string name, double quantity, MeasurementType measurement)
+        {
+            Name = name;
+            Quantity = quantity;
+            Measurement = measurement;
+        }
+
+        public enum MeasurementType
+        {
+            Pound,
+            Ounce,
+            Tablespoon,
+            Teaspoon,
+            Gram,
+            Cup,
+            Pinch
+        }
+    }
+}
---
+++ GroceryListGenerator/GroceryListGenerator/Meal.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroceryListGenerator
+{
+    class Meal
+    {
+        public string Name { get; set; }
+        public DifficultyCategory Difficulty { get; private set; }
+        public List<Ingredient> Ingredients { get; private set; }
+
+        public Meal(string name, DifficultyCategory difficulty)
+        {
+            Name = name;
+            Difficulty = difficulty;
+        }
+
+        public void AddIngredient(Ingredient ingredient)
+        {
+            Ingredients.Add(ingredient);
+        }
+
+        public void RemoveIngredient (Ingredient ingredient)
+        {
+            Ingredients.Remove(ingredient);
+        }
+
+        public void SetDifficulty(DifficultyCategory difficulty)
+        {
+            Difficulty = difficulty;
+        }
+
+        public enum DifficultyCategory
+        {
+            Easy,
+            Medium,
+            Hard
+        }
+    }
+}
