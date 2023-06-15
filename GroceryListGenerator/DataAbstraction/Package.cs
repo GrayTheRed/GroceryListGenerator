411e88d0f58a149d589df35475af7d5baf7d014a
Joshua Gray <j.d.gray@outlook.com>
2023-06-14 21:18:51 -0700

    cosmos working.  app config not working

--- GroceryListGenerator/DataAbstraction/Package.cs
+++ GroceryListGenerator/DataAbstraction/Package.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Configuration;
+using Microsoft.Azure.Cosmos;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +11,23 @@ namespace DataAbstraction
 {
     public class Package
     {
+        //TODO: Find out why config manager isn't working properly
         private static readonly string EndpointUri = ConfigurationManager.AppSettings["EndpointUri"];
         private static readonly string PrimaryKey = ConfigurationManager.AppSettings["PrimaryKey"];
-        
+        private CosmosClient cosmosClient;
+        private string databaseId = "GroceryList";
+        private string containerId = "Recipes";
+        private Database database;
+        private Container container;
+        private string trial;
+
+        public List<Meal> meals;
+
+        public Package()
+        {
+            string whatevs = ConfigurationManager.AppSettings["EndpointUri"];
+        }
+
         public void SaveRecipes(List<Recipe> recipes)
         {
 
@@ -20,12 +35,40 @@ namespace DataAbstraction
 
         public List<Meal> GetRecipes(string id)
         {
-            List<Meal> results = new List<Meal>();
-
-            results = TestRecipes();
-                        
-            return results;
+            Task.WaitAll(GetRecipesCosmos(id));
+            return meals;
         }
+
+        public async Task GetRecipesCosmos(string id)
+        {
+            await GetCosmosData(id);
+        }
+
+        public async Task GetCosmosData(string id)
+        {
+            string EndpointUri = ConfigurationManager.AppSettings["EndpointUri"];
+            string PrimaryKey = ConfigurationManager.AppSettings["PrimaryKey"];
+            cosmosClient = new CosmosClient(EndpointUri, PrimaryKey, new CosmosClientOptions() { ApplicationName = "GroceryListGenerator" });
+            container = cosmosClient.GetContainer(databaseId, containerId);
+            database = cosmosClient.GetDatabase(databaseId);
+            
+            var queryText = $"SELECT * FROM c where c.user = \"{id}\"";
+            QueryDefinition queryDefinition = new QueryDefinition(queryText);
+            FeedIterator<Meal> queryResultSetIterator = container.GetItemQueryIterator<Meal>(queryDefinition);
+            
+            meals = new List<Meal>();
+
+            while (queryResultSetIterator.HasMoreResults)
+            {
+                FeedResponse<Meal> currentResultSet = await queryResultSetIterator.ReadNextAsync();
+                foreach(Meal meal in currentResultSet)
+                {
+                    meals.Add(meal);
+                }
+            }
+        }
+
+
 
         private List<Meal> TestRecipes()
         {
